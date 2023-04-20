680b2b4b36956240940b598b09d0e4c0b611decd
DESKTOP-4DOA1T4\1 Bit PC <warr@1bit.com.ph>
2023-04-20 17:45:56 +0800

    add function to create watermark

--- Helper/helper.js
+++ Helper/helper.js
@@ -16,3 +16,29 @@ module.exports.encode = (filename) => {
     }).run();
 }
 
+var ffmpeg1 = require("ffmpeg")
+
+module.exports.createVideoWatermark = async (filename, watermarkPath = "./assets/butt.png") => {
+    try{
+
+        var video = await new ffmpeg1(`./files/${filename}`)
+        var newFilepath = `./watermarked/${filename}.mp4`
+    
+        var settings = {
+            position: "SE", // Position: NE NC NW SE SC SW C CE CW
+            margin_nord: null, // Margin nord
+            margin_sud: null, // Margin sud
+            margin_east: null, // Margin east
+            margin_west: null // Margin west
+        }
+    
+        // deletes any existing video so we can replace it
+        // fs.unlinkSync(newFilepath)
+    
+        // creates our watermarked video
+        return await video.fnAddWatermark(watermarkPath, newFilepath, settings)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
